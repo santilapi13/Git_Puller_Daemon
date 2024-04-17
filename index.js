0c34d4fca84b1d301914d9fa54076b8cd50405d2
santilapi13 <santilapiana02@gmail.com>
2024-04-17 20:16:22 -0300

    Daemon terminado en windows. Falta testeo en linux.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { spawn } = require('child_process');
 const dotenv = require('dotenv');
 const nodemailer = require('nodemailer');
 
-if (process.argv.length != 3) {
-    console.log("Usage: node index.js <notification_email>");
+if (process.argv.length != 4) {
+    console.log("Usage: node index.js <repo directory> <notification_email>");
     process.exit(1);
 }
 
@@ -26,12 +26,48 @@ function skeletonDaemon() {
     child.unref(); // Desvincular el proceso principal del hijo para que pueda ejecutarse independientemente
 }
 
-function sendEmail(transport, tweet) {
+function thereWereChanges(callback) {
+    // Ejecuta el comando git pull en el directorio del repositorio
+    const child = spawn('git', ['pull'], {
+        cwd: process.argv[2],
+        stdio: 'pipe' // Redirige la salida estándar del proceso hijo (stdout)
+    });
+
+    // Captura la salida del comando git pull
+    let output = '';
+    child.stdout.on('data', (data) => {
+        output += data.toString();
+    });
+
+    // Manejar la salida del comando git pull
+    child.on('exit', (code) => {
+        if (code === 0) {
+            if (output.includes('Already up to date')) {
+                console.log('No se detectaron cambios en el repositorio');
+                callback(false);
+            } else {
+                console.log('Cambios detectados en el repositorio');
+                callback(true);
+            }
+        } else {
+            console.log('Error al ejecutar git pull');
+            callback(false);
+        }
+    });
+}
+
+function sendEmail(transport) {
     transport.sendMail({
         from: EMAIL,
-        to: process.argv[2],
-        subject: "Notificación daemon",
-        html: tweet
+        to: process.argv[3],
+        subject: "Notificación de actualización del repositorio",
+        html: `<h1> git pull ejecutado con éxito. </h1>`
+    }, (err, info) => {
+        if (err) {
+            console.error('Error al enviar el correo electrónico:', err);
+        } else {
+            console.log('Correo electrónico enviado:', info.response);
+        }
     });
 }
 
@@ -46,10 +82,12 @@ function main() {
     });
 
     setInterval(() => {
-        tweet = `<h1> Prueba </h1>`
-        sendEmail(transport, tweet);
-    }, 10000); // Ejecutar cada 10 segundos
+        thereWereChanges((changesDetected) => {
+            if (changesDetected)
+                sendEmail(transport);
+        });
+    }, 5000); // Ejecutar cada 20 segundos
 }
 
-//skeletonDaemon();
+skeletonDaemon();
 main();
